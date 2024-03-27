3737a11381a3c12c552a4f06ac2fc47b28ed1f30
callmeaf <alikhoshkar2000@gmail.com>
2024-03-27 07:11:05 +0330

    Add getApiKey and http method in ServiceInterface

--- src/Services/V1/Contracts/SmsServiceInterface.php
+++ src/Services/V1/Contracts/SmsServiceInterface.php
@@ -2,11 +2,14 @@
 
 namespace Callmeaf\Sms\Services\V1\Contracts;
 
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
 
 interface SmsServiceInterface
 {
-    public function getApiUrl(): string;
+    public function getApiKey(): string;
+    public function getApiUrl(string $append = ''): string;
+    public function http(): PendingRequest;
     public function sendViaPattern(string $pattern,string $mobile,array $values): Response;
     public function verifyOtpPattern(): string;
 }
