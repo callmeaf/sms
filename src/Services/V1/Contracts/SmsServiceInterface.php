e95ec9cfab0b4af595880f6c7468854b61db9572
callmeaf <alikhoshkar2000@gmail.com>
2024-03-28 11:54:35 +0330

    Add send and multi send method

--- src/Services/V1/Contracts/SmsServiceInterface.php
+++ src/Services/V1/Contracts/SmsServiceInterface.php
@@ -10,6 +10,8 @@ interface SmsServiceInterface
     public function getApiKey(): string;
     public function getApiUrl(string $append = ''): string;
     public function http(): PendingRequest;
+    public function send(string $mobile,string $message): Response;
+    public function multiSend(array|string $mobiles,array|string $messages,array|string $senders): Response;
     public function sendViaPattern(string $pattern,string $mobile,array $values): Response;
     public function verifyOtpPattern(): string;
 }
